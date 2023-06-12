e12cd9b4f2a64de25569d791ae6dc059ea766326
fanpu <fanpu@users.noreply.github.com>
2023-06-12 13:12:15 +0000

    Deploying to gh-pages from @ fanpu/website@97bc735a91087858f570e4394b0ea21092eb1bc4 🚀

---
+++ assets/img/convert_avif.py
@@ -0,0 +1,21 @@
+import os
+import pathlib
+
+print("Run this from the directory that you want to convert image files for")
+
+p = pathlib.Path(".")
+
+target_suffix = ".avif"
+image_exts = [
+    ".jpg",
+    ".JPG",
+    ".jpeg",
+    ".png",
+]
+
+for f in p.rglob("*"):
+    if os.path.isfile(f):
+        if f.suffix in image_exts:
+            new_path = f.with_suffix(target_suffix)
+            print(new_path)
+            os.system(f"convert {f} {new_path}")
